d05eb075b4961a3f2e8843ba974ddb8c9e40da9e
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2022-03-01 15:44:16 -0500

    Debug tty com

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,34 @@
-/* 
- * Auteurs: Jean-Samuel Lauzon    
- * Date: Fevrier 2022
-*/
-
-/*------------------------------ Librairies ---------------------------------*/
 #include <Arduino.h>
-#include <ArduinoJson.h>
 
 /*------------------------------ Constantes ---------------------------------*/
 
 #define BAUD 9600        // Frequence de transmission serielle
 
-/*---------------------------- Variables globales ---------------------------*/
+int incomingByte = 0; // for incoming serial data
+char msg[100];
+int i = 0;
 
-volatile bool shouldSend_ = false;  // Drapeau prêt à envoyer un message
-volatile bool shouldRead_ = false;  // Drapeau prêt à lire un message
-
-int ledState = 0;
-int potValue = 0;
-
-int pinLED = 7;
-int pinPOT = A0;
-
-
-/*------------------------- Prototypes de fonctions -------------------------*/
-void sendMsg(); 
-void readMsg();
-void serialEvent();
 /*---------------------------- Fonctions "Main" -----------------------------*/
 
 void setup() {
   Serial.begin(BAUD);               // Initialisation de la communication serielle
-  pinMode(pinLED, OUTPUT);
-  digitalWrite(pinLED, ledState);
 }
 
 /* Boucle principale (infinie) */
 void loop() {
+    // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
 
-  if(shouldRead_){
-    readMsg();
-    sendMsg();
-  }
-
-  potValue = analogRead(pinPOT);
-  //Serial.println(potValue);          // debug
-  delay(10);  // delais de 10 ms
-}
-
-/*---------------------------Definition de fonctions ------------------------*/
-
-void serialEvent() { shouldRead_ = true; }
-
-
-/*---------------------------Definition de fonctions ------------------------
-Fonction d'envoi
-Entrée : Aucun
-Sortie : Aucun
-Traitement : Envoi du message
------------------------------------------------------------------------------*/
-void sendMsg() {
-  StaticJsonDocument<500> doc;
-  // Elements du message
-  doc["time"] = millis();
-  doc["analog"] = potValue;
-
-  // Serialisation
-  serializeJson(doc, Serial);
-
-  // Envoie
-  Serial.println();
-  shouldSend_ = false;
-}
-
-/*---------------------------Definition de fonctions ------------------------
-Fonction de reception
-Entrée : Aucun
-Sortie : Aucun
-Traitement : Réception du message
------------------------------------------------------------------------------*/
-void readMsg(){
-  // Lecture du message Json
-  StaticJsonDocument<500> doc;
-  JsonVariant parse_msg;
-
-  // Lecture sur le port Seriel
-  DeserializationError error = deserializeJson(doc, Serial);
-  shouldRead_ = false;
-
-  // Si erreur dans le message
-  if (error) {
-    Serial.print("deserialize() failed: ");
-    Serial.println(error.c_str());
-    return;
-  }
-  
-  // Analyse des éléments du message message
-  parse_msg = doc["led"];
-  if (!parse_msg.isNull()) {
-    // mettre la led a la valeur doc["led"]
-    digitalWrite(pinLED,doc["led"].as<bool>());
+    // say what you got:
+    if(incomingByte != 0){
+        msg[i] = incomingByte;
+        i++;
+    }
+    if(incomingByte == 10){
+        delay(10);
+        Serial.println(msg);
+    }
   }
 }
